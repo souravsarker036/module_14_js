e3faca9a33f00adcf958c1dad21ee92a924b246f
Sourav Sarker <souravsarker036@gmail.com>
2025-07-30 04:51:21 +0600

    comparison

---
+++ compare_var.js
@@ -0,0 +1,17 @@
+// console.log(5>10)
+// console.log(5<10)
+// console.log(5==10)
+// console.log(11>=10)
+
+// var apple = 20;
+// var mango = 30;
+// console.log(apple>mango)
+
+// == is meaning both side are equal.. and its just check its value not data type
+// console.log(10 == '10')
+// === when given === then its checks its valu and data type
+// console.log(10 === '10')
+// != checking both side r not equal and its checks only value not data type 
+// console.log( 10 !='10')
+// !== checking both side r not equal and its checks value and data type both 
+console.log( 10 !== '10')

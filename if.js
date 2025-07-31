cc2bbf89cd1b63eb6a7a28f0205303ee5b1067ab
Sourav Sarker <souravsarker036@gmail.com>
2025-08-01 01:11:36 +0600

    first-if-else

---
+++ if.js
@@ -0,0 +1,15 @@
+if(3<10){
+    console.log('i go to market')
+}
+var biriyaniPrice = 150;
+if(biriyaniPrice < 200)
+{
+    console.log('i will buy biriyani')
+}
+var temperature = 80;
+if(temperature > 70){
+    console.log('the biriyani is hot')
+}
+else{
+    console.log('biriyani is cold')
+}

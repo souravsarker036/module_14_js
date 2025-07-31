563b9399f4f0d8e1acaa5011f5bf6ea361d15b37
Sourav Sarker <souravsarker036@gmail.com>
2025-08-01 01:31:35 +0600

    double condition

---
+++ multiple.js
@@ -0,0 +1,22 @@
+const salary = 25000;
+const isBCS = true;
+const height = 75;
+// if(salary > 20000 && isBCS == true || height > 70){
+//     console.log('su---patro')
+// }
+// else{
+//     console.log('find another one')
+// }
+//if(salary > 20000 && isBCS == false || height > 70){
+//     console.log('su---patro')
+// }
+// else{
+//     console.log('find another one')
+// }
+// if((salary > 20000 && isBCS == false) || height > 80){
+if((salary > 20000 && isBCS == false) || height > 70){
+    console.log('su---patro')
+}
+else{
+    console.log('find another one')
+}
